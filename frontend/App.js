6dd561df5a6b715262af897535e084f121a433ce
EstefaniaPerezY <a01639270@itesm.mx>
2022-10-27 13:34:01 -0500

    carpetas nuevas, front añadido
    se movieron los archivos anteriores a una nueva carpeta llamada "backend", y también se agregó el frontend

---
+++ frontend/App.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+function App() {
+    return (
+      <>
+      <h1>Bienvenido</h1>
+      <Link to = "/users">
+      <button type="button" class="btn btn-primary">Go to Users</button>
+      </Link>
+      </>
+      
+    );
+}
+  
+export default App;
